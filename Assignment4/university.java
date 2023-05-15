a7a8b0674ef2bb2b7a9723e0d5ad7c66c78718c4
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-05-15 14:05:09 +0600

    Create university.java

---
+++ Assignment4/university.java
@@ -0,0 +1,50 @@
+package assignment_3;
+import java.util.*;
+
+public class University {
+	private ArrayList<Person> list;
+	public University(ArrayList<Person> persons) {
+		this.list = persons;
+	}
+	 public String getFullNames() {
+		 ArrayList<String>names = new ArrayList<>();
+		 for(Person per : list) {
+			 names.add(per.getFullName());
+		 }
+		 String out="";
+		 for(int i=0; i<names.size(); i++) {
+			 if(i!=names.size()-1)
+				 out = out + names.get(i) + ",";
+			 else
+				 out = out + names.get(i);
+		 }
+		 return out;
+	}
+	 
+	 public Person getOldestPerson() {
+	        Person oldestPerson = null;
+	        for (Person person : list) {
+	            if (oldestPerson == null || person.getAge() > oldestPerson.getAge()) {
+	                oldestPerson = person;
+	            }
+	        }
+	        return oldestPerson;
+	    }
+	 public Person getYoungestPerson() {
+	        Person youngestPerson = new Person(list.get(0).getFirstName(), list.get(0).getLastName(), list.get(0).age);
+	        for (Person person : list) {
+	            if (youngestPerson == null || person.getAge() < youngestPerson.getAge()) {
+	                youngestPerson = person;
+	            }
+	        }
+	        return youngestPerson;
+	    }
+	 public ArrayList<Person> getVoters(){
+		 ArrayList<Person> canVote = new ArrayList<Person>();
+		 for(Person x : list) {
+			 if(x.canVote())
+				 canVote.add(x);
+		 }
+		return canVote;
+	 }
+}
