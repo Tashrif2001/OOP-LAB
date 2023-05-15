4853fea22f22e0c4839c63d194c20e5ccff007a6
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-05-15 13:18:19 +0600

    Create Student.java

---
+++ Assignment_3/Student.java
@@ -0,0 +1,13 @@
+package assignment_3;
+
+public class Student extends Person{
+
+	public Student(String firstName, String lastName, int age) {
+		super(firstName, lastName, age);	
+	}
+	@Override
+	public String getFullName() {
+		return String.format("%s, %s", super.getLastName(), super.getFirstName());
+	}
+	
+}
